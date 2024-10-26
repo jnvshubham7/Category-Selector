400d2211d13a041f68cec4fbadf0c3ec4b558c64
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2024-10-26 15:53:05 +0530

    Refactor category selector styles and add shadows

--- lib/main.dart
+++ lib/main.dart
@@ -75,11 +75,9 @@ class _CategorySelectorState extends State<CategorySelector> {
         backgroundColor: Colors.white,
         scrolledUnderElevation: 0,
         leading: Container(
-          // margin: const EdgeInsets.only(left: 10.0),
           child: IconButton(
             icon: Image.asset('assets/back.png', width: 32, height: 32),
             onPressed: () {},
-            // splashRadius: 10,
             tooltip: 'Go back',
           ),
         ),
@@ -107,19 +105,16 @@ class _CategorySelectorState extends State<CategorySelector> {
       body: Column(
         children: [
           Container(
-            color: Colors.grey.shade300, // Light grey color for divider
-            height: 1, // Divider thickness
+            color: Colors.grey.shade300,
+            height: 1,
           ),
           Expanded(
             child: Row(
               children: [
                 Container(
-                  // padding: EdgeInsets.symmetric(vertical: 8.0),
                   width: 100,
                   decoration: BoxDecoration(
                     color: const Color(0xFFF8F7FE),
-
-                    // add shsow in right side
                     boxShadow: [
                       BoxShadow(
                         color: Colors.black.withOpacity(0.06),
@@ -127,10 +122,6 @@ class _CategorySelectorState extends State<CategorySelector> {
                         offset: const Offset(0, 2),
                       ),
                     ],
-
-                    // border: Border(
-                    //   right: BorderSide(color: Colors.grey[300]!, width: 1),
-                    // ),
                   ),
                   child: ScrollConfiguration(
                     behavior: ScrollConfiguration.of(context).copyWith(
@@ -147,12 +138,9 @@ class _CategorySelectorState extends State<CategorySelector> {
                           },
                           child: Container(
                             decoration: BoxDecoration(
-                              // selcted color white
-                              // unslected color background: #F8F7FE;
                               color: index == _selectedCategoryIndex
                                   ? Colors.white
                                   : const Color(0xFFF8F7FE),
-
                               border: Border(
                                 left: BorderSide(
                                   color: index == _selectedCategoryIndex
@@ -165,8 +153,6 @@ class _CategorySelectorState extends State<CategorySelector> {
                                   width: 1,
                                 ),
                               ),
-
-                              // add shadow right side
                               boxShadow: [
                                 BoxShadow(
                                   color: Colors.black.withOpacity(0.06),
@@ -180,10 +166,7 @@ class _CategorySelectorState extends State<CategorySelector> {
                             child: Column(
                               children: [
                                 CircleAvatar(
-                                  // backgroundColor: Color(0xFFE5E5E5),
                                   backgroundImage: AssetImage(
-
-                                      // 'assets/kids.png',
                                       categorizedItems[index]["icon"]),
                                 ),
                                 const SizedBox(height: 8),
@@ -218,7 +201,6 @@ class _CategorySelectorState extends State<CategorySelector> {
                     child: ListView(
                       children: [
                         Padding(
-                          //
                           padding: const EdgeInsets.fromLTRB(15, 15, 8, 8),
                           child: Row(
                             children: [
@@ -233,8 +215,8 @@ class _CategorySelectorState extends State<CategorySelector> {
                               const SizedBox(width: 8),
                               const Expanded(
                                 child: Padding(
-                                  padding: EdgeInsets.only(
-                                      top: 8.0, right: 10.0),
+                                  padding:
+                                      EdgeInsets.only(top: 8.0, right: 10.0),
                                   child: Divider(
                                     color: Colors.black26,
                                     thickness: 1,
@@ -283,81 +265,74 @@ class CategorySection extends StatelessWidget {
             ),
           ),
         ),
-
-        Padding(padding: const EdgeInsets.fromLTRB(20, 8, 8, 8),
-
-
-         child:SizedBox(
-          height: 250,
-          child: SingleChildScrollView(
-            physics: const NeverScrollableScrollPhysics(),
-            child: GridView.builder(
-              shrinkWrap: true,
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 8, 8, 8),
+          child: SizedBox(
+            height: 250,
+            child: SingleChildScrollView(
               physics: const NeverScrollableScrollPhysics(),
-              scrollDirection: Axis.vertical,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                mainAxisSpacing: 10,
-                
-                crossAxisSpacing: 10,
-                childAspectRatio: 0.75,
-                // this ratio means 
+              child: GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                scrollDirection: Axis.vertical,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  childAspectRatio: 0.75,
+                ),
+                itemCount: 6,
+                itemBuilder: (context, index) {
+                  return Container(
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: ClipRRect(
+                            borderRadius: const BorderRadius.vertical(
+                                top: Radius.circular(8)),
+                            child: Image.asset(
+                              'assets/image.png',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 67.98,
+                          height: 21.24,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.white, width: 0.5),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.06),
+                                blurRadius: 6.0,
+                                offset: const Offset(0, 2),
+                              ),
+                            ],
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Fusion Wear',
+                              style: TextStyle(
+                                fontFamily: 'Manrope',
+                                fontWeight: FontWeight.w500,
+                                fontSize: 10,
+                                height: 1.8,
+                                color: Colors.black.withOpacity(0.8),
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
               ),
-              itemCount: 6,
-              itemBuilder: (context, index) {
-                return Container(
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: ClipRRect(
-                          borderRadius:
-                              const BorderRadius.vertical(top: Radius.circular(8)),
-                          child: Image.asset(
-                            'assets/image.png',
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: 67.98,
-                        height: 21.24,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(color: Colors.white, width: 0.5),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.06),
-                              blurRadius: 6.0,
-                              offset: const Offset(0, 2),
-                            ),
-                          ],
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: Center(
-                          child: Text(
-                            'Fusion Wear',
-                            style: TextStyle(
-                              fontFamily: 'Manrope',
-                              fontWeight: FontWeight.w500,
-                              fontSize: 10,
-                              height: 1.8,
-                              color: Colors.black.withOpacity(0.8),
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
             ),
           ),
         ),
-        
-        )
-        
-       
       ],
     );
   }
